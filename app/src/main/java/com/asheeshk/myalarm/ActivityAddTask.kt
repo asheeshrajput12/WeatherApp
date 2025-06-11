d81869099d04f9109e32e58e8952b787ae9f6da0
Asheesh Kumar <asheeshkumar75497@gmail.com>
2025-06-11 08:50:35 +0530

    project setup.

--- app/src/main/java/com/asheeshk/myalarm/ActivityAddTask.kt
+++ app/src/main/java/com/asheeshk/myalarm/ActivityAddTask.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -19,11 +17,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -34,26 +30,19 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -68,17 +57,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextLayoutInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -87,10 +72,8 @@ import androidx.core.content.ContextCompat
 import com.asheeshk.myalarm.ui.theme.MyAlarmTheme
 import com.asheeshk.myalarm.ui.theme.Pink10
 import com.asheeshk.myalarm.ui.theme.Pink80
-import com.asheeshk.myalarm.ui.theme.Purple80
 import kotlinx.coroutines.launch
 import java.time.LocalTime
-import kotlin.math.abs
 
 class ActivityAddTask : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -412,7 +395,6 @@ fun getAlarmRingtones(context: Context): List<Pair<String, Uri>> {
     val ringtoneManager = RingtoneManager(context)
     ringtoneManager.setType(RingtoneManager.TYPE_ALARM)
     val cursor = ringtoneManager.getCursor()
-
     val ringtones = mutableListOf<Pair<String, Uri>>()
     while (cursor.moveToNext()) {
         val title = cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX)
